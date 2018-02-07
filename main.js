2148a08a88a085edbfbb6b301ac152249e443c89
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-02-06 20:39:35 -0500

    Switchover to node with hidden MTAKey

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-var ProtoBuf = require("protobufjs");
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+const config = require('./config');
+
+var requestSettings = {
+  method: 'GET',
+  url: `http://datamine.mta.info/mta_esi.php?key=${config.mtaKey}`,
+  encoding: null
+};
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
