fa8fbb89641941994645cb3d4ab77af003cab715
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-02-11 21:38:18 -0500

    Add stops function

---
+++ stops.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const readLine = require('readline');
+
+const rl = readLine.createInterface({
+  input: fs.createReadStream('./static/stops.txt')
+});
+
+const stops = [];
+rl.on('line', (line) => {
+  const stop = {};
+  const lineData = line.split(",");
+  stop.stop_id = lineData[0];
+  stop.stop_name = lineData[2];
+  stop.lat = lineData[4];
+  stop.long = lineData[5];
+  stop.parent = lineData[9];
+  stops.push(stop);
+});
+//
+// rl.on('close')
