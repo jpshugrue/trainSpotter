a80a87b5a9e194ce32bfbc37f80c8f40047f1dff
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-02-11 22:15:05 -0500

    Stops before change to readfile

--- stops.js
+++ stops.js
@@ -1,20 +1,30 @@
 const fs = require("fs");
 const readLine = require('readline');
 
-const rl = readLine.createInterface({
-  input: fs.createReadStream('./static/stops.txt')
-});
+const populateStops = () => {
+  const rl = readLine.createInterface({
+    input: fs.createReadStream('./static/stops.txt')
+  });
 
-const stops = [];
-rl.on('line', (line) => {
-  const stop = {};
-  const lineData = line.split(",");
-  stop.stop_id = lineData[0];
-  stop.stop_name = lineData[2];
-  stop.lat = lineData[4];
-  stop.long = lineData[5];
-  stop.parent = lineData[9];
-  stops.push(stop);
-});
-//
-// rl.on('close')
+  const stops = [];
+  rl.on('line', (line) => {
+    const stop = {};
+    const lineData = line.split(",");
+    stop.stop_id = lineData[0];
+    stop.stop_name = lineData[2];
+    stop.lat = lineData[4];
+    stop.long = lineData[5];
+    stop.parent = lineData[9];
+    stops.push(stop);
+  });
+
+  rl.on('close', () => {
+    // console.log(stops);
+  });
+};
+
+module.exports = populateStops;
+
+// rl.on('close', () => {
+//   // console.log(stops);
+// });
