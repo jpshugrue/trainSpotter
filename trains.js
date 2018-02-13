0d1b4a454ec69d488577985b9d4938e9fce89ded
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-02-12 20:44:25 -0500

    Add webpack and protobufjs

--- trains.js
+++ trains.js
@@ -1,26 +1,26 @@
-const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+// const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const request = require('request');
 const config = require('./config');
-const populateStops = require('./stops');
+// const populateStops = require('./stops');
 
 module.exports = class Trains {
 
   constructor() {
     this.trains = [];
-    populateStops((stops) => {
-      this.stops = stops;
-    });
+    // populateStops((stops) => {
+    //   this.stops = stops;
+    // });
   }
 
+  // 1,2,3,4,5,6,S(42ndStShuttle) = '1'
+  // A,C,E = '26'
+  // N,Q,R,W = '16'
+  // B,D,F,M = '21'
+  // L = '2'
+  // G = '31'
+  // J, Z = '36'
+  // 7 = '51' (In Beta)
   pullData(trainLine) {
-    // 1,2,3,4,5,6,S(42ndStShuttle) = '1'
-    // A,C,E = '26'
-    // N,Q,R,W = '16'
-    // B,D,F,M = '21'
-    // L = '2'
-    // G = '31'
-    // J, Z = '36'
-    // 7 = '51' (In Beta)
     const requestSettings = {
       method: 'GET',
       url: `http://datamine.mta.info/mta_esi.php?key=${config.mtaKey}&feed_id=${trainLine}`,
@@ -28,10 +28,11 @@ module.exports = class Trains {
     };
     request(requestSettings, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-        feed.entity.forEach((entity) => {
-          this.trains.push(entity);
-        });
+
+        // const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+        // feed.entity.forEach((entity) => {
+        //   this.trains.push(entity);
+        // });
       }
     });
   }
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+  entry: './trains.js',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
